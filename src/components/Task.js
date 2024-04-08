ca2fd76005d575b5672e89959950ea04584b0e47
Zédourado <103081912+Hammes01@users.noreply.github.com>
2024-04-08 10:57:29 -0300

    Criando os components
    Adicionando Task.js

---
+++ src/components/Task.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import { ListItem, ListItemText, ListItemSecondaryAction, IconButton, Checkbox } from '@mui/material';
+import { Delete, Edit, Done } from '@mui/icons-material';
